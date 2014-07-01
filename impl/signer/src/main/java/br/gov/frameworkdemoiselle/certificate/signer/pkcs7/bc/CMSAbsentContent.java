9f4cac10691f29adabf529e3c050efa2480d1d23
07721825741 <07721825741@serpro-1538650>
2014-07-01 19:18:28 -0300

    Alteracao da interface implementada

--- impl/signer/src/main/java/br/gov/frameworkdemoiselle/certificate/signer/pkcs7/bc/CMSAbsentContent.java
+++ impl/signer/src/main/java/br/gov/frameworkdemoiselle/certificate/signer/pkcs7/bc/CMSAbsentContent.java
@@ -2,21 +2,25 @@ package br.gov.frameworkdemoiselle.certificate.signer.pkcs7.bc;
 
 import java.io.IOException;
 import java.io.OutputStream;
-
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.bouncycastle.cms.CMSException;
-import org.bouncycastle.cms.CMSProcessable;
+import org.bouncycastle.cms.CMSTypedData;
 
-public class CMSAbsentContent implements CMSProcessable {
+public class CMSAbsentContent implements CMSTypedData {
 
-	public CMSAbsentContent() {
-	}
+    @Override
+    public ASN1ObjectIdentifier getContentType() {
+        return null;
+    }
 
-	@Override
-	public void write(OutputStream zOut) throws IOException, CMSException {
-	}
+    @Override
+    public void write(OutputStream out) throws IOException, CMSException {
 
-	@Override
-	public Object getContent() {
-		return null;
-	}
+    }
+
+    @Override
+    public Object getContent() {
+        return null;
+    }
+
 }
