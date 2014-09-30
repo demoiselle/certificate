9788b1133f09ea33ffa00b9df436e8a8ba40b458
07721825741 <07721825741@serpro-1538650>
2014-09-30 15:16:20 -0300

    Refatoração

--- impl/signer/src/main/java/br/gov/frameworkdemoiselle/certificate/signer/Signer.java
+++ impl/signer/src/main/java/br/gov/frameworkdemoiselle/certificate/signer/Signer.java
@@ -3,38 +3,37 @@
  * Copyright (C) 2010 SERPRO
  * ----------------------------------------------------------------------------
  * This file is part of Demoiselle Framework.
- * 
+ *
  * Demoiselle Framework is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License version 3
  * as published by the Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License version 3
  * along with this program; if not,  see <http://www.gnu.org/licenses/>
  * or write to the Free Software Foundation, Inc., 51 Franklin Street,
  * Fifth Floor, Boston, MA  02110-1301, USA.
  * ----------------------------------------------------------------------------
  * Este arquivo é parte do Framework Demoiselle.
- * 
+ *
  * O Framework Demoiselle é um software livre; você pode redistribuí-lo e/ou
  * modificá-lo dentro dos termos da GNU LGPL versão 3 como publicada pela Fundação
  * do Software Livre (FSF).
- * 
+ *
  * Este programa é distribuído na esperança que possa ser útil, mas SEM NENHUMA
  * GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer MERCADO ou
  * APLICAÇÃO EM PARTICULAR. Veja a Licença Pública Geral GNU/LGPL em português
  * para maiores detalhes.
- * 
+ *
  * Você deve ter recebido uma cópia da GNU LGPL versão 3, sob o título
  * "LICENCA.txt", junto com esse programa. Se não, acesse <http://www.gnu.org/licenses/>
  * ou escreva para a Fundação do Software Livre (FSF) Inc.,
  * 51 Franklin St, Fifth Floor, Boston, MA 02111-1301, USA.
  */
-
 package br.gov.frameworkdemoiselle.certificate.signer;
 
 import java.security.PrivateKey;
@@ -46,59 +45,84 @@ import java.security.PublicKey;
  */
 public interface Signer {
 
-	/**
-	 * Indica qual o Provider será utilizado.
-	 */
-	abstract public void setProvider(Provider provider);
+    /**
+     * Indica qual o Provider será utilizado.
+     *
+     * @param provider
+     */
+    abstract public void setProvider(Provider provider);
 
-	/**
-	 * Chave privada necessária para a criptografia assimétrica
-	 */
-	abstract public void setPrivateKey(PrivateKey privateKey);
+    /**
+     * Chave privada necessária para a criptografia assimétrica
+     *
+     * @param privateKey
+     */
+    abstract public void setPrivateKey(PrivateKey privateKey);
 
-	/**
-	 * Chave publica necessária para a criptografia assimétrica
-	 */
-	abstract public void setPublicKey(PublicKey publicKey);
+    /**
+     * Chave publica necessária para a criptografia assimétrica
+     *
+     * @param publicKey
+     */
+    abstract public void setPublicKey(PublicKey publicKey);
 
-	/**
-	 * Algoritmo de Assinatura. Ex: SHA1withRSA
-	 */
-	abstract public void setAlgorithm(String algorithm);
+    /**
+     * Algoritmo de Assinatura. Ex: SHA1withRSA
+     *
+     * @param algorithm
+     */
+    abstract public void setAlgorithm(String algorithm);
 
-	/**
-	 * Algoritmo pré-defido no enum. Compatíveis com ICP-Brasil
-	 */
-	abstract public void setAlgorithm(SignerAlgorithmEnum algorithm);
+    /**
+     * Algoritmo pré-defido no enum. Compatíveis com ICP-Brasil
+     *
+     * @param algorithm
+     */
+    abstract public void setAlgorithm(SignerAlgorithmEnum algorithm);
 
-	/**
-	 * Método de assinatura digital.
-	 */
-	abstract public byte[] signer(byte[] content);
+    /**
+     * Método de assinatura digital.
+     *
+     * @param content
+     * @return
+     */
+    abstract public byte[] signer(byte[] content);
 
-	/**
-	 * Método de validação da assinatura.
-	 */
-	abstract public boolean check(byte[] content, byte[] signed);
+    /**
+     * Método de validação da assinatura.
+     *
+     * @param content
+     * @param signed
+     * @return
+     */
+    abstract public boolean check(byte[] content, byte[] signed);
 
-	/**
-	 * Retorna o provider.
-	 */
-	abstract public Provider getProvider();
+    /**
+     * Retorna o provider.
+     *
+     * @return
+     */
+    abstract public Provider getProvider();
 
-	/**
-	 * Retorna a chave privada.
-	 */
-	abstract public PrivateKey getPrivateKey();
+    /**
+     * Retorna a chave privada.
+     *
+     * @return
+     */
+    abstract public PrivateKey getPrivateKey();
 
-	/**
-	 * Retorna o algoritmo.
-	 */
-	abstract public String getAlgorithm();
+    /**
+     * Retorna o algoritmo.
+     *
+     * @return
+     */
+    abstract public String getAlgorithm();
 
-	/**
-	 * Retorna a chave publica.
-	 */
-	abstract public PublicKey getPublicKey();
+    /**
+     * Retorna a chave publica.
+     *
+     * @return
+     */
+    abstract public PublicKey getPublicKey();
 
 }
