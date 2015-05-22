f49325eaac7d03a17408c58b81d8727a27ebd673
Ednara Oliveira <ednaraoliveira@gmail.com>
2015-05-22 16:47:15 -0300

    Adicionando o certificado da Homologação SerproACFv4Homolog

--- impl/ca-icp-brasil-homologacao/src/main/java/br/gov/frameworkdemoiselle/certificate/ca/provider/hom/HomologacaoProviderCA.java
+++ impl/ca-icp-brasil-homologacao/src/main/java/br/gov/frameworkdemoiselle/certificate/ca/provider/hom/HomologacaoProviderCA.java
@@ -53,9 +53,11 @@ public class HomologacaoProviderCA implements ProviderCA {
             InputStream raizDeHomologacaoSERPRO = HomologacaoProviderCA.class.getClassLoader().getResourceAsStream("trustedca/RaizdeHomologacaoSERPRO.cer");
             InputStream ACSERPROACFv3Homologacao = HomologacaoProviderCA.class.getClassLoader().getResourceAsStream("trustedca/ACSERPROACFv3Homologacao.cer");
             InputStream intermediariaHOMv2 = HomologacaoProviderCA.class.getClassLoader().getResourceAsStream("trustedca/IntermediariaHOMv2.cer");
+            InputStream ACSERPROACFv4Homologacao = HomologacaoProviderCA.class.getClassLoader().getResourceAsStream("trustedca/serproACFv4Homolog.cer");
             result.add((X509Certificate) CertificateFactory.getInstance("X509").generateCertificate(raizDeHomologacaoSERPRO));
             result.add((X509Certificate) CertificateFactory.getInstance("X509").generateCertificate(ACSERPROACFv3Homologacao));
             result.add((X509Certificate) CertificateFactory.getInstance("X509").generateCertificate(intermediariaHOMv2));
+            result.add((X509Certificate) CertificateFactory.getInstance("X509").generateCertificate(ACSERPROACFv4Homologacao));
         } catch (Throwable error) {
             error.printStackTrace();
             return null;
