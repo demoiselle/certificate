71613bec4c40e5517d87ad6398e15c8975ff297f
Ednara Oliveira <ednaraoliveira@gmail.com>
2016-03-28 15:21:54 -0300

    Componente para aplicação desktop

---
+++ impl/desktop/src/main/java/br/gov/frameworkdemoiselle/certificate/ui/util/Utils.java
@@ -0,0 +1,261 @@
+/*
+ * Demoiselle Framework
+ * Copyright (C) 2010 SERPRO
+ * ----------------------------------------------------------------------------
+ * This file is part of Demoiselle Framework.
+ * 
+ * Demoiselle Framework is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License version 3
+ * as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License version 3
+ * along with this program; if not,  see <http://www.gnu.org/licenses/>
+ * or write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA  02110-1301, USA.
+ * ----------------------------------------------------------------------------
+ * Este arquivo é parte do Framework Demoiselle.
+ * 
+ * O Framework Demoiselle é um software livre; você pode redistribuí-lo e/ou
+ * modificá-lo dentro dos termos da GNU LGPL versão 3 como publicada pela Fundação
+ * do Software Livre (FSF).
+ * 
+ * Este programa é distribuído na esperança que possa ser útil, mas SEM NENHUMA
+ * GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer MERCADO ou
+ * APLICAÇÃO EM PARTICULAR. Veja a Licença Pública Geral GNU/LGPL em português
+ * para maiores detalhes.
+ * 
+ * Você deve ter recebido uma cópia da GNU LGPL versão 3, sob o título
+ * "LICENCA.txt", junto com esse programa. Se não, acesse <http://www.gnu.org/licenses/>
+ * ou escreva para a Fundação do Software Livre (FSF) Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02111-1301, USA.
+ */
+
+package br.gov.frameworkdemoiselle.certificate.ui.util;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.StringWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.cert.Certificate;
+import java.security.spec.EncodedKeySpec;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.bouncycastle.openssl.PEMWriter;
+
+/**
+ * @author SUPST/STDCS
+*/
+public class Utils {
+
+    private final int BUFFER_SIZE = 4096;
+
+    /**
+     *
+     * @param content O conteudo a ser enviado
+     * @param UrlToUpload A url para onde o conteudo sera enviado
+     */
+    public void uploadToURL(byte[] content, String UrlToUpload) {
+        try {
+            System.out.println("br.gov.serpro.certificate.ui.util.Utils.uploadToURL()");
+
+            ByteArrayInputStream in = new ByteArrayInputStream(content);
+            URL url = new URL(UrlToUpload);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setDoOutput(true);
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "application/octet-stream");
+
+            try (OutputStream out = con.getOutputStream()) {
+                copy(in, out);
+                out.flush();
+            }
+
+            int responseCode = con.getResponseCode();
+            System.out.println("Response Code...: " + responseCode);
+            if (responseCode != HttpURLConnection.HTTP_OK) {
+                Logger.getLogger(Utils.class.getName()).log(Level.SEVERE, "Server returned non-OK code: {0}", responseCode);
+            }
+
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(Utils.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Utils.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    /**
+     *
+     * @param UrlToDownload
+     * @return
+     */
+    public byte[] downloadFromUrl(String UrlToDownload) {
+        ByteArrayOutputStream outputStream = null;
+        try {
+            System.out.println("br.gov.serpro.certificate.ui.util.Utils.downloadFromUrl()");
+            URL url = new URL(UrlToDownload);
+            outputStream = new ByteArrayOutputStream();
+            byte[] chunk = new byte[BUFFER_SIZE];
+            int bytesRead;
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            int responseCode = con.getResponseCode();
+            System.out.println("Response Code...: " + responseCode);
+            if (responseCode != HttpURLConnection.HTTP_OK) {
+                Logger.getLogger(Utils.class.getName()).log(Level.SEVERE, "Server returned non-OK code: {0}", responseCode);
+                throw new ConectionException("Server returned non-OK code: " + responseCode);
+            }
+            else {
+                InputStream stream = con.getInputStream();
+                
+                while ((bytesRead = stream.read(chunk)) > 0) {
+                    outputStream.write(chunk, 0, bytesRead);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return outputStream.toByteArray();
+    }
+
+    /**
+     * Read the given binary file, and return its contents as a byte array.
+     *
+     * @param file Caminho e nome do arquivo
+     * @return Conteudo lido
+     */
+    public static byte[] readContentFromDisk(String file) {
+        File f = new File(file);
+
+        byte[] result = new byte[(int) f.length()];
+        try {
+            InputStream in = null;
+            try {
+                int totalBytesRead = 0;
+                in = new BufferedInputStream(new FileInputStream(f));
+                while (totalBytesRead < result.length) {
+                    int bytesRemaining = result.length - totalBytesRead;
+                    int bytesRead = in.read(result, totalBytesRead, bytesRemaining);
+                    if (bytesRead > 0) {
+                        totalBytesRead = totalBytesRead + bytesRead;
+                    }
+                }
+
+            } finally {
+                in.close();
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return result;
+    }
+
+    /**
+     *
+     * @param content Conteudo a ser gravado
+     * @param file Caminho e nome do arquivo
+     */
+    public void writeContentToDisk(byte[] content, String file) {
+        try {
+            File f = new File(file);
+            FileOutputStream os = new FileOutputStream(f);
+            os.write(content);
+            os.flush();
+            os.close();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    /**
+     *
+     * @param input
+     * @param output
+     * @return
+     * @throws IOException
+     */
+    private long copy(InputStream input, OutputStream output) throws IOException {
+        byte[] buffer = new byte[BUFFER_SIZE];
+        long count = 0L;
+        int n = 0;
+        while (-1 != (n = input.read(buffer))) {
+            output.write(buffer, 0, n);
+            count += n;
+        }
+        return count;
+    }
+
+    /**
+     * Create a random 1024 bit RSA key pair
+     *
+     * @return
+     * @throws java.lang.Exception
+     */
+    public static KeyPair generateRSAKeyPair() throws Exception {
+        KeyPairGenerator kpGen = KeyPairGenerator.getInstance("RSA", "BC");
+        kpGen.initialize(2048, new SecureRandom());
+        return kpGen.generateKeyPair();
+    }
+
+    /**
+     * Converts a {@link X509Certificate} instance into a Base-64 encoded string
+     * (PEM format).
+     *
+     * @param x509Cert A X509 Certificate instance
+     * @return PEM formatted String
+     * @throws java.io.IOException
+     */
+    public static String convertToBase64PEMString(Certificate x509Cert) throws IOException {
+        StringWriter sw = new StringWriter();
+        try (PEMWriter pw = new PEMWriter(sw)) {
+            pw.writeObject(x509Cert);
+        }
+        return sw.toString();
+    }
+
+    public static PublicKey reconstructPublicKey(String algorithm, byte[] pub_key) {
+        PublicKey public_key = null;
+
+        try {
+            KeyFactory kf = KeyFactory.getInstance(algorithm, "BC");
+            EncodedKeySpec pub_key_spec = new X509EncodedKeySpec(pub_key);
+            public_key = kf.generatePublic(pub_key_spec);
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("Could not reconstruct the public key, the given algorithm oculd not be found.");
+        } catch (InvalidKeySpecException e) {
+            System.out.println("Could not reconstruct the public key");
+        } catch (NoSuchProviderException ex) {
+            Logger.getLogger(Utils.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return public_key;
+    }
+
+}
---
+++ impl/desktop/src/main/java/br/gov/frameworkdemoiselle/certificate/ui/view/Principal.java
@@ -0,0 +1,344 @@
+/*
+ * Demoiselle Framework
+ * Copyright (C) 2010 SERPRO
+ * ----------------------------------------------------------------------------
+ * This file is part of Demoiselle Framework.
+ * 
+ * Demoiselle Framework is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License version 3
+ * as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License version 3
+ * along with this program; if not,  see <http://www.gnu.org/licenses/>
+ * or write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA  02110-1301, USA.
+ * ----------------------------------------------------------------------------
+ * Este arquivo é parte do Framework Demoiselle.
+ * 
+ * O Framework Demoiselle é um software livre; você pode redistribuí-lo e/ou
+ * modificá-lo dentro dos termos da GNU LGPL versão 3 como publicada pela Fundação
+ * do Software Livre (FSF).
+ * 
+ * Este programa é distribuído na esperança que possa ser útil, mas SEM NENHUMA
+ * GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer MERCADO ou
+ * APLICAÇÃO EM PARTICULAR. Veja a Licença Pública Geral GNU/LGPL em português
+ * para maiores detalhes.
+ * 
+ * Você deve ter recebido uma cópia da GNU LGPL versão 3, sob o título
+ * "LICENCA.txt", junto com esse programa. Se não, acesse <http://www.gnu.org/licenses/>
+ * ou escreva para a Fundação do Software Livre (FSF) Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02111-1301, USA.
+ */
+
+package br.gov.frameworkdemoiselle.certificate.ui.view;
+
+import java.awt.Cursor;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.security.KeyStore;
+
+import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.WindowConstants;
+import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+
+import br.gov.frameworkdemoiselle.certificate.CertificateValidatorException;
+import br.gov.frameworkdemoiselle.certificate.keystore.loader.DriverNotAvailableException;
+import br.gov.frameworkdemoiselle.certificate.keystore.loader.InvalidPinException;
+import br.gov.frameworkdemoiselle.certificate.keystore.loader.KeyStoreLoader;
+import br.gov.frameworkdemoiselle.certificate.keystore.loader.KeyStoreLoaderException;
+import br.gov.frameworkdemoiselle.certificate.keystore.loader.PKCS11NotFoundException;
+import br.gov.frameworkdemoiselle.certificate.keystore.loader.factory.KeyStoreLoaderFactory;
+import br.gov.frameworkdemoiselle.certificate.ui.action.FrameExecute;
+import br.gov.frameworkdemoiselle.certificate.ui.config.FrameConfig;
+import br.gov.frameworkdemoiselle.certificate.ui.factory.FrameExecuteFactory;
+import br.gov.frameworkdemoiselle.certificate.ui.handler.PinCallbackHandler;
+import br.gov.frameworkdemoiselle.certificate.ui.tiny.Item;
+
+/**
+ * @author SUPST/STDCS
+*/
+public class Principal extends javax.swing.JFrame {
+
+	private static final long serialVersionUID = 1L;
+	
+	private JButton btnCancelar;
+	private JButton btnExecutar;
+	private JPanel panelbottom;
+	private JPanel paneltop;
+	private JScrollPane scrollPane;
+	private JTable tableCertificates;
+	
+	KeyStore keystore = null;
+	private boolean loaded = false;
+	String alias = "";
+	String className = "";
+	CertificadoModel certificateModel;
+
+	/**
+	 * Creates new form NovoJFrame
+	 */
+	public Principal() {
+		initComponents();
+		className = System.getProperty("jnlp.myClassName");
+
+		if (className == null || className.isEmpty()) {
+			className = "br.gov.serpro.certificate.ui.user.App";
+		}
+		System.out.println("Utilizando implementacao da classe [" + className+ "]");
+
+		keystore = this.getKeyStore();// Recupera o repositorio de certificados digitais
+
+		certificateModel = new CertificadoModel();
+		certificateModel.populate(keystore);
+		tableCertificates.setModel(certificateModel);
+
+		if (tableCertificates.getRowCount() == 0) {
+			btnExecutar.setEnabled(false);
+		} else {
+			tableCertificates.setRowSelectionInterval(0, 0);
+		}
+
+		alias = this.getAlias();
+
+		tableCertificates.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+
+		// Dimensiona cada coluna separadamente
+		TableColumn tableColumn1 = tableCertificates.getColumnModel().getColumn(0);
+		tableColumn1.setPreferredWidth(200);
+
+		TableColumn tableColumn2 = tableCertificates.getColumnModel().getColumn(1);
+		tableColumn2.setPreferredWidth(140);
+
+		TableColumn tableColumn3 = tableCertificates.getColumnModel().getColumn(2);
+		tableColumn3.setPreferredWidth(140);
+
+		TableColumn tableColumn4 = tableCertificates.getColumnModel().getColumn(3);
+		tableColumn4.setPreferredWidth(300);
+
+		this.setLocationRelativeTo(null); // Centraliza o frame
+	}
+
+	private void initComponents() {
+
+		paneltop = new JPanel();
+		scrollPane = new JScrollPane();
+		tableCertificates = new JTable();
+		panelbottom = new JPanel();
+		btnExecutar = new JButton();
+		btnCancelar = new JButton();
+
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setLocation(new Point(0, 0));
+		setResizable(false);
+		setTitle(FrameConfig.LABEL_DIALOG_FRAME_TITLE.getValue());
+
+		paneltop.setBorder(BorderFactory.createTitledBorder(
+				BorderFactory.createEtchedBorder(),
+				FrameConfig.CONFIG_DIALOG_TABLE_LABEL.getValue(),
+				TitledBorder.DEFAULT_JUSTIFICATION,
+				TitledBorder.DEFAULT_POSITION,
+				new java.awt.Font(FrameConfig.CONFIG_DIALOG_TABLE_LABEL_FONT.getValue(), FrameConfig.CONFIG_DIALOG_TABLE_LABEL_FONT_STYLE.getValueInt(), FrameConfig.CONFIG_DIALOG_TABLE_LABEL_FONT_SIZE.getValueInt()))); // NOI18N
+
+		scrollPane.setAutoscrolls(true);
+		scrollPane.setViewportView(tableCertificates);
+
+		tableCertificates.setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1));
+		tableCertificates.setModel(new DefaultTableModel(
+				new Object[][] { { null, null, null, null },
+						{ null, null, null, null }, { null, null, null, null },
+						{ null, null, null, null } }, new String[] { "Title 1",
+						"Title 2", "Title 3", "Title 4" }));
+		tableCertificates.setFillsViewportHeight(true);
+		tableCertificates.setRowHeight(FrameConfig.CONFIG_DIALOG_TABLE_CERTIFICATES_ROW_HEIGHT.getValueInt());
+
+		GroupLayout paneltopLayout = new GroupLayout(paneltop);
+		paneltop.setLayout(paneltopLayout);
+		
+		paneltopLayout.setHorizontalGroup(paneltopLayout.createParallelGroup(Alignment.LEADING)
+				.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE,	FrameConfig.CONFIG_DIALOG_TABLE_CERTIFICATES_WIDTH.getValueInt(), Short.MAX_VALUE));
+		
+		paneltopLayout.setVerticalGroup(paneltopLayout.createParallelGroup(Alignment.LEADING)
+				.addComponent(scrollPane,GroupLayout.PREFERRED_SIZE,FrameConfig.CONFIG_DIALOG_TABLE_CERTIFICATES_HEIGHT.getValueInt(),GroupLayout.PREFERRED_SIZE));
+	
+		panelbottom.setBorder(BorderFactory.createEtchedBorder());
+
+		btnExecutar.setText(FrameConfig.LABEL_DIALOG_BUTTON_RUN.getValue());
+		btnExecutar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				btnExecutarActionPerformed(evt);
+			}
+		});
+
+		btnCancelar.setText(FrameConfig.LABEL_DIALOG_BUTTON_CANCEL.getValue());
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				btnCancelarActionPerformed(evt);
+			}
+		});
+		
+		GroupLayout panelbottomLayout = new GroupLayout(panelbottom);
+		panelbottom.setLayout(panelbottomLayout);
+		panelbottomLayout.setHorizontalGroup(panelbottomLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(panelbottomLayout.createSequentialGroup()
+								.addComponent(btnExecutar,GroupLayout.PREFERRED_SIZE, FrameConfig.CONFIG_DIALOG_BUTTON_RUN_WIDTH.getValueInt(), GroupLayout.PREFERRED_SIZE)
+								.addComponent(btnCancelar, GroupLayout.PREFERRED_SIZE, FrameConfig.CONFIG_DIALOG_BUTTON_CANCEL_WIDTH.getValueInt(), GroupLayout.PREFERRED_SIZE)
+						));
+		panelbottomLayout.setVerticalGroup(panelbottomLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(panelbottomLayout.createSequentialGroup()
+								.addContainerGap()
+								.addGroup(panelbottomLayout.createParallelGroup(Alignment.BASELINE)
+										.addComponent(btnExecutar, GroupLayout.PREFERRED_SIZE, FrameConfig.CONFIG_DIALOG_BUTTON_RUN_HEIGHT.getValueInt(), GroupLayout.PREFERRED_SIZE)
+										.addComponent(btnCancelar, GroupLayout.PREFERRED_SIZE, FrameConfig.CONFIG_DIALOG_BUTTON_CANCEL_HEIGHT.getValueInt(), GroupLayout.PREFERRED_SIZE)
+								)
+								.addContainerGap()
+						));
+	
+		GroupLayout layout = new GroupLayout(getContentPane());
+		getContentPane().setLayout(layout);
+		layout.setHorizontalGroup(layout.createParallelGroup(Alignment.LEADING)
+				.addGroup(layout.createSequentialGroup()
+								.addContainerGap()
+								.addGroup(layout.createParallelGroup(Alignment.LEADING,	false)
+												.addComponent(paneltop, GroupLayout.DEFAULT_SIZE,GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+												.addComponent(panelbottom, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+								.addContainerGap(GroupLayout.DEFAULT_SIZE,Short.MAX_VALUE)));
+		layout.setVerticalGroup(layout.createParallelGroup(Alignment.LEADING)
+				.addGroup(layout.createSequentialGroup()
+								.addContainerGap()
+								.addComponent(paneltop, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addPreferredGap(ComponentPlacement.RELATED)
+								.addComponent(panelbottom, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)));
+
+		pack();
+	}
+
+	private void btnExecutarActionPerformed(java.awt.event.ActionEvent evt) {
+		FrameExecute frameExecute = FrameExecuteFactory.factory(className);
+		frameExecute.execute(keystore, alias, this);
+
+	}
+
+	private void btnCancelarActionPerformed(java.awt.event.ActionEvent evt) {
+		FrameExecute frameExecute = FrameExecuteFactory.factory(className);
+		frameExecute.cancel(keystore, alias, this);
+	}
+
+	/**
+	 * Retorna o keystore do dispositivo a partir do valor de pin
+	 *
+	 * @return
+	 */
+	public KeyStore getKeyStore() {
+		try {
+			Cursor hourGlassCursor = new Cursor(Cursor.WAIT_CURSOR);
+			setCursor(hourGlassCursor);
+			KeyStoreLoader loader = KeyStoreLoaderFactory
+					.factoryKeyStoreLoader();
+			loader.setCallbackHandler(new PinCallbackHandler());
+			keystore = loader.getKeyStore();
+			loaded = true;
+			return keystore;
+
+		} catch (DriverNotAvailableException e) {
+			showFailDialog(FrameConfig.MESSAGE_ERROR_DRIVER_NOT_AVAILABLE
+					.getValue());
+		} catch (PKCS11NotFoundException e) {
+			showFailDialog(FrameConfig.MESSAGE_ERROR_PKCS11_NOT_FOUND
+					.getValue());
+		} catch (CertificateValidatorException e) {
+			showFailDialog(FrameConfig.MESSAGE_ERROR_LOAD_TOKEN.getValue());
+		} catch (InvalidPinException e) {
+			showFailDialog(FrameConfig.MESSAGE_ERROR_INVALID_PIN.getValue());
+		} catch (KeyStoreLoaderException ke) {
+			showFailDialog(ke.getMessage());
+		} catch (Exception ex) {
+			showFailDialog(FrameConfig.MESSAGE_ERROR_UNEXPECTED.getValue());
+		} finally {
+			Cursor hourGlassCursor = new Cursor(Cursor.DEFAULT_CURSOR);
+			setCursor(hourGlassCursor);
+		}
+		return null;
+	}
+
+	/**
+	 * Obtem o apelido associado a um certificado
+	 *
+	 * @return O apelido associado ao certificado
+	 */
+	public String getAlias() {
+		if (tableCertificates.getModel().getRowCount() != 0) {
+			int row = tableCertificates.getSelectedRow();
+			Item item = (Item) tableCertificates.getModel().getValueAt(row, 0);
+			return item.getAlias();
+		} else {
+			return "";
+		}
+	}
+
+	/**
+	 * Exibe as mensagens de erro
+	 *
+	 * @param message
+	 */
+	private void showFailDialog(String message) {
+		JOptionPane.showMessageDialog(this, message,
+				FrameConfig.LABEL_DIALOG_OPTION_PANE_TITLE.getValue(),
+				JOptionPane.ERROR_MESSAGE);
+	}
+	
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Principal().setVisible(true);
+            }
+        });
+    }
+	
+}
