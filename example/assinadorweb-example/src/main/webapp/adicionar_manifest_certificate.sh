f336389a493d1a139f42f34d03de4695a672e9be
esaito <emerson.saito@gmail.com>
2017-11-09 14:38:38 -0200

    scripts para assinar jar.

---
+++ example/assinadorweb-example/src/main/webapp/adicionar_manifest_certificate.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+
+# modifica Manifest.MF dos arquivos .jar
+echo "*** modificando os arquivos .jar ..."
+
+# jar ufm jar-file manifest-addition.txt
+
+
+jar ufm assinadorweb-desktop-1.2.2.jar manifest-addition-certificate.txt
+# jar ufm demoiselle-certificate-ca-icpbrasil-1.2.2-SNAPSHOT.jar manifest-addition-certificate.txt
+# jar ufm demoiselle-certificate-ca-icpbrasil-homologacao-1.2.2-SNAPSHOT.jar manifest-addition-certificate.txt
+# jar ufm demoiselle-certificate-core-1.2.2-SNAPSHOT.jar manifest-addition-certificate.txt
+# jar ufm demoiselle-certificate-criptography-1.2.2-SNAPSHOT.jar manifest-addition-certificate.txt
+# jar ufm demoiselle-certificate-desktop-1.2.2-SNAPSHOT.jar manifest-addition-certificate.txt
+# jar ufm demoiselle-certificate-signer-1.2.2-SNAPSHOT.jar manifest-addition-certificate.txt
+# jar ufm bcmail-jdk15-1.45.jar manifest-addition-certificate.txt
+# jar ufm bcprov-jdk15-1.45.jar manifest-addition-certificate.txt
+
+
+
+read -s -n 1 -p "Press any key to continue…"
+
+echo "*** Fim!"
+
+
+
+
+
---
+++ example/assinadorweb-example/src/main/webapp/jar_signer_com_token.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+clear
+
+## SETAR O JAVA HOME em .bashrc
+echo $JAVA_HOME
+
+
+#Colocar o Pin do Token aqui
+PASSWORD="senha"
+
+#NAO ALTERAR!
+DSANAME="SERPRO"
+
+#Apontar para o caminho onde estão os jars  que devem ser assinados
+JARPATH="/home/80621732915/git/certificate/example/assinadorweb-example/src/main/webapp"
+
+#lista os dados do token, inclusive o apelido
+keytool -keystore NONE -storetype PKCS11 -providerClass sun.security.pkcs11.SunPKCS11 -providerArg drivers.config -storepass $PASSWORD -list
+
+
+for jarfile in $(ls $JARPATH/*.jar); do
+    jarfile_signed="${jarfile%.jar}-assinado.jar"
+    echo "Gerando jar assinado para $jarfile em $jarfile_signed"
+    #Nome do arquivo a ser assinado
+    #JARFILE="security-applet-customizada-1.0.0.jar"
+
+    #nome ddo arquivo depois de assinado
+    #JARFILESIGNED="security-applet-customizada-1.0.0-assinado.jar"
+
+    #apelido do certificado, rodar o script uma primeira vez no console e copiar o apelido cara este script
+    ALIAS="SERVICO FEDERAL DE PROCESSAMENTO DE DADOS SERPRO:CETEC's Autoridade Certificadora do SERPRO Final v3 ID"
+	    
+    #assina o jar
+    jarsigner -verbose -J-Djava.security.debug=sunpkcs11 -keystore NONE -storetype PKCS11 -providerClass sun.security.pkcs11.SunPKCS11 -providerArg drivers.config -storepass $PASSWORD -sigfile $DSANAME -signedjar $jarfile_signed -verbose $jarfile "$ALIAS"
+done
+
+exit 0
